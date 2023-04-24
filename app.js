f4f96b686dd517b586cb28f2d85be5f0897ba359
jamesjoel <jamesjoelj@gmail.com>
2023-04-24 15:10:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+//mongodb+srv://jamesjoel:<password>@cluster0.8lcs5.mongodb.net/?retryWrites=true&w=majority
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://jamesjoel:jamesjoel@cluster0.8lcs5.mongodb.net/mydb");
+
+const Teacher = mongoose.Schema({
+    name : String,
+    age : String
+});
+
+const TeacherModel = mongoose.model("teacher", Teacher);
+
+
+app.get("/api/teacher", async(req, res)=>{
+    let result = await TeacherModel.find();
+    console.log(result);
+    res.send(result);
+})
+
+const port = process.env.PORT || 3001;
+app.listen(port, ()=>{
+    console.log("server running with port ", port)
+})
